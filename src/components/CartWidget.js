4da0af195170ca8fa0ea600b20bd55996784734c
Marini Marcos <mmarini95@hotmail.com>
2022-12-25 22:54:31 -0300

    PreEntrega1 - Marini Marcos

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CartWidget = () => {
   return (
     <>
-      <a href="">
+      <a href=""> 1
       <span className='material-icons'>shopping_cart</span>  
       </a>
     </>
